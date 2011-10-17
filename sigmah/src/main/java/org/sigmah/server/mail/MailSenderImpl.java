74e0b51012b034618eba3251d12cebad2bdfc798
Olivier Sarrat <olivier.sarrat@laposte.net>
2011-10-17 09:32:06 +0000

    Add SMTP port management to enable sending mail with sigmah.org SMTP server.
    Fixes #376

--- sigmah/src/main/java/org/sigmah/server/mail/MailSenderImpl.java
+++ sigmah/src/main/java/org/sigmah/server/mail/MailSenderImpl.java
@@ -17,13 +17,15 @@ import com.google.inject.Inject;
 
 public class MailSenderImpl implements MailSender {
 
-    final public static String HOST_NAME_KEY = "mail.hostname";
+    final public static String HOST_NAME_KEY = "mail.hostname";   
+    final public static String PORT_KEY = "mail.port";
     final public static String FROM_ADDRESS_KEY = "mail.from.address";
     final public static String FROM_NAME_KEY = "mail.from.name";
     final public static String FROM_AUTH_USERNAME_KEY = "mail.auth.username";
     final public static String FROM_AUTH_PASSWORD_KEY = "mail.auth.password";
 
     final private static String DEFAULT_HOST_NAME = "localhost";
+    final private static String DEFAULT_PORT = 25;
     final private static String DEFAULT_ADDRESS = "mailer@sigmah.org";
     final private static String DEFAULT_NAME = "Sigmah";
 
@@ -35,6 +37,7 @@ public class MailSenderImpl implements MailSender {
     public void send(Email email) throws EmailException {
 
         email.setHostName(properties.getProperty(HOST_NAME_KEY, DEFAULT_HOST_NAME));
+        email.setSmtpPort(properties.getProperty(PORT_KEY, DEFAULT_PORT));
         email.setFrom(properties.getProperty(FROM_ADDRESS_KEY, DEFAULT_ADDRESS),
                 properties.getProperty(FROM_NAME_KEY, DEFAULT_NAME));
 
