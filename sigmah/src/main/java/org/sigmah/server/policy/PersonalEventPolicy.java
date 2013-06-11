6d06f2bcbd62557d2b81357f2b99ad7818f80071
JB Guerline <jbguerline@live.fr>
2013-06-11 08:51:20 +0000

    Displays events on the right day at the right time according to the client's time zone (fixes issue #557)

--- sigmah/src/main/java/org/sigmah/server/policy/PersonalEventPolicy.java
+++ sigmah/src/main/java/org/sigmah/server/policy/PersonalEventPolicy.java
@@ -5,67 +5,73 @@
 
 package org.sigmah.server.policy;
 
-import com.extjs.gxt.ui.client.widget.form.Time;
-import com.google.inject.Inject;
+import java.io.Serializable;
 import java.util.Date;
+
 import org.sigmah.client.page.project.calendar.ProjectCalendarPresenter.CalendarWrapper;
 import org.sigmah.server.dao.PersonalEventDAO;
 import org.sigmah.shared.domain.User;
 import org.sigmah.shared.domain.calendar.PersonalEvent;
 
+import com.google.inject.Inject;
+
 /**
- *
+ * 
  * @author Raphaël Calabro (rcalabro@ideia.fr)
  */
 public class PersonalEventPolicy implements EntityPolicy<PersonalEvent> {
-    final PersonalEventDAO dao;
+	final PersonalEventDAO dao;
 
-    @Inject
-    public PersonalEventPolicy(PersonalEventDAO dao) {
-        this.dao = dao;
-    }
+	@Inject
+	public PersonalEventPolicy(PersonalEventDAO dao) {
+		this.dao = dao;
+	}
 
-    @Override
-    public Object create(User user, PropertyMap properties) {
-        final PersonalEvent event = new PersonalEvent();
+	@Override
+	public Object create(User user, PropertyMap properties) {
+		final PersonalEvent event = new PersonalEvent();
 
-        event.setDateCreated(new Date());
-        fillEvent(event, properties);
+		event.setDateCreated(new Date());
+		fillEvent(event, properties);
 
-        dao.persist(event);
+		dao.persist(event);
 
-        return event.getId();
-    }
+		return event.getId();
+	}
 
-    @Override
-    public void update(User user, Object entityId, PropertyMap changes) {
-        final PersonalEvent event = dao.findById((Integer)entityId);
-        fillEvent(event, changes);
+	@Override
+	public void update(User user, Object entityId, PropertyMap changes) {
+		final PersonalEvent event = dao.findById((Integer) entityId);
+		fillEvent(event, changes);
 
-        dao.merge(event);
-    }
+		dao.merge(event);
+	}
 
-    private void fillEvent(PersonalEvent event, PropertyMap properties) {
-        final CalendarWrapper calendar = (CalendarWrapper) properties.get("calendarId");
-        event.setCalendarId((Integer) calendar.getCalendar().getIdentifier());
-        
-        event.setSummary((String) properties.get("summary"));
-        event.setDescription((String) properties.get("description"));
+	@SuppressWarnings("deprecation")
+	private void fillEvent(PersonalEvent event, PropertyMap properties) {
+		final CalendarWrapper calendar = (CalendarWrapper) properties.get("calendarId");
+		event.setCalendarId((Integer) calendar.getCalendar().getIdentifier());
 
-        final Date day = (Date) properties.get("date");
-        final Time startHour = (Time) properties.get("startDate");
-        final Time endHour = (Time) properties.get("endDate");
+		event.setSummary((String) properties.get("summary"));
+		event.setDescription((String) properties.get("description"));
 
-        if(startHour != null) {
-            event.setStartDate(new Date(day.getYear(), day.getMonth(), day.getDate(), startHour.getHour(), startHour.getMinutes()));
-            if(endHour != null)
-                event.setEndDate(new Date(day.getYear(), day.getMonth(), day.getDate(), endHour.getHour(), endHour.getMinutes()));
-            else
-                event.setEndDate(null);
-        } else {
-            event.setStartDate(new Date(day.getYear(), day.getMonth(), day.getDate()));
-            event.setEndDate(new Date(day.getYear(), day.getMonth(), day.getDate()+1));
-        }
-    }
+		final Date day = (Date) properties.get("date");
+		final Serializable startHourSerialized = properties.get("startDate");
+		final Serializable endHourSerialized = properties.get("endDate");
+
+		if (startHourSerialized != null) {
+			final Date startHour = new Date((Long) startHourSerialized);
+			event.setStartDate(startHour);
+			if (endHourSerialized != null) {
+				final Date endHour = new Date((Long) endHourSerialized);
+				event.setEndDate(endHour);
+			} else {
+				event.setEndDate(null);
+			}
+		} else {
+			event.setStartDate(new Date(day.getYear(), day.getMonth(), day.getDate()));
+			event.setEndDate(new Date(day.getYear(), day.getMonth(), day.getDate() + 1));
+		}
+	}
 
 }
