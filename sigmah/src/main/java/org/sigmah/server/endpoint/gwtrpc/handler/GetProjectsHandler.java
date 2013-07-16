23c4774165c08d97da5cec5fde060782867fbed9
JB Guerline <jbguerline@live.fr>
2013-07-16 09:55:04 +0000

    Allow user to link his projects to projects from the same OrgUnit (fixes issue #588)

--- sigmah/src/main/java/org/sigmah/server/endpoint/gwtrpc/handler/GetProjectsHandler.java
+++ sigmah/src/main/java/org/sigmah/server/endpoint/gwtrpc/handler/GetProjectsHandler.java
@@ -30,158 +30,152 @@ import com.google.inject.Inject;
 
 public class GetProjectsHandler implements CommandHandler<GetProjects> {
 
-    private final static Log LOG = LogFactory.getLog(GetProjectsHandler.class);
+	private final static Log LOG = LogFactory.getLog(GetProjectsHandler.class);
 
-    private final EntityManager em;
-    private final ProjectMapper mapper;
+	private final EntityManager em;
+	private final ProjectMapper mapper;
 
-    @Inject
-    public GetProjectsHandler(EntityManager em, ProjectMapper mapper) {
-        this.em = em;
-        this.mapper = mapper;
-    }
+	@Inject
+	public GetProjectsHandler(EntityManager em, ProjectMapper mapper) {
+		this.em = em;
+		this.mapper = mapper;
+	}
 
-    /**
-     * Gets the projects list from the database.
-     * 
-     * @param cmd
-     * @param user
-     * 
-     * @return a {@link CommandResult} object containing the
-     *         {@link ProjectListResult} object.
-     */
-    @Override
-    @SuppressWarnings("unchecked")
-    public CommandResult execute(GetProjects cmd, User user) throws CommandException {
+	/**
+	 * Gets the projects list from the database.
+	 * 
+	 * @param cmd
+	 * @param user
+	 * 
+	 * @return a {@link CommandResult} object containing the
+	 *         {@link ProjectListResult} object.
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public CommandResult execute(GetProjects cmd, User user) throws CommandException {
 
-        // Disable the ActivityInfo filter on Userdatabase.
-        org.hibernate.Session session = ((HibernateEntityManager) em).getSession();
-        session.disableFilter("userVisible");
+		// Disable the ActivityInfo filter on Userdatabase.
+		org.hibernate.Session session = ((HibernateEntityManager) em).getSession();
+		session.disableFilter("userVisible");
 
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("[execute] Gets projects: " + cmd + ".");
-        }
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("[execute] Gets projects: " + cmd + ".");
+		}
 
-        // Retrieves command parameters.
-        final HashSet<Project> projects = new HashSet<Project>();
-        final ProjectModelType modelType = cmd.getModelType();
+		// Retrieves command parameters.
+		final HashSet<Project> projects = new HashSet<Project>();
+		final ProjectModelType modelType = cmd.getModelType();
 
-        // ---------------
-        // Projects which I own or I manage.
-        // ---------------
+		// ---------------
+		// Projects which I own or I manage.
+		// ---------------
 
-        if (cmd.getViewOwnOrManage()) {
-            final Query ownerManagerQuery =
-                    em.createQuery("SELECT p FROM Project p WHERE p.owner = :ouser OR p.manager = :muser");
-            ownerManagerQuery.setParameter("ouser", user);
-            ownerManagerQuery.setParameter("muser", user);
-            for (final Project p : (List<Project>) ownerManagerQuery.getResultList()) {
-                projects.add(p);
-            }
-        }
-        // ---------------
-        // Projects in my visible organization units.
-        // ---------------
+		if (cmd.getViewOwnOrManage()) {
+			final Query ownerManagerQuery = em
+			                .createQuery("SELECT p FROM Project p WHERE p.owner = :ouser OR p.manager = :muser");
+			ownerManagerQuery.setParameter("ouser", user);
+			ownerManagerQuery.setParameter("muser", user);
+			for (final Project p : (List<Project>) ownerManagerQuery.getResultList()) {
+				projects.add(p);
+			}
+		}
+		// ---------------
+		// Projects in my visible organization units.
+		// ---------------
 
-        final List<Integer> ids = cmd.getOrgUnitsIds();
+		final List<Integer> ids = cmd.getOrgUnitsIds();
 
-        // Use a set to be avoid duplicated entries.
-        final HashSet<OrgUnit> units = new HashSet<OrgUnit>();
+		// Use a set to be avoid duplicated entries.
+		final HashSet<OrgUnit> units = new HashSet<OrgUnit>();
 
-        // Checks if there is at least one org unit id specified.
-        if (ids == null) {
+		// Checks if there is at least one org unit id specified.
+		if (ids == null) {
 
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("[execute] No org unit specified, gets all projects for the user org unit.");
-            }
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("[execute] No org unit specified, gets all projects for the user org unit.");
+			}
 
-            // Crawl the org units hierarchy from the user root org unit.
-            GetProjectHandler.crawlUnits(user.getOrgUnitWithProfiles().getOrgUnit(), units, false);
-        } else {
+			// Crawl the org units hierarchy from the user root org unit.
+			GetProjectHandler.crawlUnits(user.getOrgUnitWithProfiles().getOrgUnit(), units, true);
+		} else {
 
-            // Crawl the org units hierarchy from each specified org unit.
-            OrgUnit unit;
-            for (final Integer id : ids) {
-                if ((unit = em.find(OrgUnit.class, id)) != null) {
-                    GetProjectHandler.crawlUnits(unit, units, true);
-                }
-            }
-        }
+			// Crawl the org units hierarchy from each specified org unit.
+			OrgUnit unit;
+			for (final Integer id : ids) {
+				if ((unit = em.find(OrgUnit.class, id)) != null) {
+					GetProjectHandler.crawlUnits(unit, units, true);
+				}
+			}
+		}
 
-        // Retrieves all the corresponding org units.
-        for (final OrgUnit unit : units) {
+		// Retrieves all the corresponding org units.
+		for (final OrgUnit unit : units) {
 
-            // Builds and executes the query.
-            final Query query =
-                    em.createQuery("SELECT p FROM Project p WHERE :unit MEMBER OF p.partners");
-            query.setParameter("unit", unit);
+			// Builds and executes the query.
+			final Query query = em.createQuery("SELECT p FROM Project p WHERE :unit MEMBER OF p.partners");
+			query.setParameter("unit", unit);
 
-            int count = 0;
-            final List<Project> listResults = (List<Project>) query.getResultList();
-            for (final Project p : listResults) {
+			int count = 0;
+			final List<Project> listResults = (List<Project>) query.getResultList();
+			for (final Project p : listResults) {
 
-                if (modelType == null) {
-                    projects.add(p);
-                    count++;
-                }
-                // Filters by model type.
-                else {
-                    if (p.getProjectModel().getVisibility(user.getOrganization()) == modelType) {
-                        projects.add(p);
-                        count++;
-                    }
-                }
-            }
+				if (modelType == null) {
+					projects.add(p);
+					count++;
+				}
+				// Filters by model type.
+				else {
+					if (p.getProjectModel().getVisibility(user.getOrganization()) == modelType) {
+						projects.add(p);
+						count++;
+					}
+				}
+			}
 
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("[execute] Found "
-                    + count
-                    + "/"
-                    + listResults.size()
-                    + " projects for org unit #"
-                    + unit.getName()
-                    + ".");
-            }
-        }
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("[execute] Found " + count + "/" + listResults.size() + " projects for org unit #"
+				                + unit.getName() + ".");
+			}
+		}
 
-        // ---------------
-        // Mapping and return.
-        // ---------------
+		// ---------------
+		// Mapping and return.
+		// ---------------
 
-        final ProjectListResult result = new ProjectListResult();
+		final ProjectListResult result = new ProjectListResult();
 
-        switch (cmd.getReturnType()) {
-            case PROJECT:
-                // Not implemented.
-                break;
-            case ID:
+		switch (cmd.getReturnType()) {
+		case PROJECT:
+			// Not implemented.
+			break;
+		case ID:
 
-                final ArrayList<Integer> projectsIds = new ArrayList<Integer>();
-                for (final Project project : projects) {
-                    projectsIds.add(project.getId());
-                }
+			final ArrayList<Integer> projectsIds = new ArrayList<Integer>();
+			for (final Project project : projects) {
+				projectsIds.add(project.getId());
+			}
 
-                result.setListProjectsIds(projectsIds);
-                break;
+			result.setListProjectsIds(projectsIds);
+			break;
 
-            case PROJECT_LIGHT:
-            default:
+		case PROJECT_LIGHT:
+		default:
 
-                // Mapping into DTO objects
-                final ArrayList<ProjectDTOLight> projectDTOList = new ArrayList<ProjectDTOLight>();
-                for (final Project project : projects) {
-                    final ProjectDTOLight pLight = mapper.map(project, true);
-                    projectDTOList.add(pLight);
-                }
+			// Mapping into DTO objects
+			final ArrayList<ProjectDTOLight> projectDTOList = new ArrayList<ProjectDTOLight>();
+			for (final Project project : projects) {
+				final ProjectDTOLight pLight = mapper.map(project, true);
+				projectDTOList.add(pLight);
+			}
 
-                result.setListProjectsLightDTO(projectDTOList);
-                break;
-        }
+			result.setListProjectsLightDTO(projectDTOList);
+			break;
+		}
 
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("[execute] Found " + projects.size() + " project(s).");
-        }
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("[execute] Found " + projects.size() + " project(s).");
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
