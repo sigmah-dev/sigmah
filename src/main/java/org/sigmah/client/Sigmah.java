a9f676b44dad74d51ffd78aedea8d9265855b553
daeke <daeke@192.168.106.219>
2016-01-05 11:32:17 +0100

    Added the version number for the Profiler.

--- src/main/java/org/sigmah/client/Sigmah.java
+++ src/main/java/org/sigmah/client/Sigmah.java
@@ -17,6 +17,12 @@ import com.google.gwt.core.client.GWT.UncaughtExceptionHandler;
  * @author Tom Miette (tmiette@ideia.fr)
  */
 public class Sigmah implements EntryPoint {
+	
+	/**
+	 * Version number. Hard coded to avoid querying the server to identify
+	 * the current version number.
+	 */
+	public static final String VERSION = "2.0.2";
 
 	/**
 	 * GIN injector.
