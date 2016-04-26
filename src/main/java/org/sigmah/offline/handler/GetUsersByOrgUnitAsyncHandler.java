155a0f2ccbf9570adcbe5f0441a33c506d91dd4c
Aurélien Ponçon <apo@atolcd.com>
2016-04-26 13:01:44 +0200

    Fix of not complete user list in team member page in offline mode
    Change-Id: I5fc2bdf67321e0a74eaeb971343dee1980795237

--- src/main/java/org/sigmah/offline/handler/GetUsersByOrgUnitAsyncHandler.java
+++ src/main/java/org/sigmah/offline/handler/GetUsersByOrgUnitAsyncHandler.java
@@ -3,7 +3,6 @@ package org.sigmah.offline.handler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +11,6 @@ import org.sigmah.offline.dao.OrgUnitAsyncDAO;
 import org.sigmah.offline.dao.UserAsyncDAO;
 import org.sigmah.offline.dispatch.AsyncCommandHandler;
 import org.sigmah.offline.dispatch.OfflineExecutionContext;
-import org.sigmah.server.domain.OrgUnit;
 import org.sigmah.shared.command.GetUsersByOrgUnit;
 import org.sigmah.shared.command.result.Authentication;
 import org.sigmah.shared.command.result.ListResult;
@@ -47,13 +45,12 @@ public class GetUsersByOrgUnitAsyncHandler implements AsyncCommandHandler<GetUse
 	}
 
 	private Set<Integer> traverseOrgUnitIds(OrgUnitDTO orgUnit) {
-		if (orgUnit.getChildrenOrgUnits() == null) {
-			return Collections.singleton(orgUnit.getId());
-		}
-		Set<Integer> orgUnitIds = new HashSet<Integer>(orgUnit.getChildrenOrgUnits().size());
-		orgUnitIds.add(orgUnit.getId());
-		for (OrgUnitDTO child : orgUnit.getChildrenOrgUnits()) {
-			orgUnitIds.addAll(traverseOrgUnitIds(child));
+		Set<Integer> orgUnitIds = new HashSet<Integer>();
+		OrgUnitDTO parent = orgUnit;
+		while (parent != null) {
+			orgUnitIds.add(parent.getId());
+
+			parent = parent.getParentOrgUnit();
 		}
 		return orgUnitIds;
 	}
--- src/main/java/org/sigmah/server/handler/GetUsersByOrgUnitHandler.java
+++ src/main/java/org/sigmah/server/handler/GetUsersByOrgUnitHandler.java
@@ -1,19 +1,22 @@
 package org.sigmah.server.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
 import org.sigmah.server.dao.OrgUnitDAO;
 import org.sigmah.server.dao.UserDAO;
 import org.sigmah.server.dispatch.impl.UserDispatch.UserExecutionContext;
+import org.sigmah.server.domain.OrgUnit;
 import org.sigmah.server.domain.User;
 import org.sigmah.server.handler.base.AbstractCommandHandler;
 import org.sigmah.shared.command.GetUsersByOrgUnit;
 import org.sigmah.shared.command.result.ListResult;
 import org.sigmah.shared.dispatch.CommandException;
 import org.sigmah.shared.dto.UserDTO;
-
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import org.sigmah.shared.dto.orgunit.OrgUnitDTO;
 
 public class GetUsersByOrgUnitHandler extends AbstractCommandHandler<GetUsersByOrgUnit, ListResult<UserDTO>> {
 	private final OrgUnitDAO orgUnitDAO;
@@ -31,8 +34,19 @@ public class GetUsersByOrgUnitHandler extends AbstractCommandHandler<GetUsersByO
 		List<User> users = userDAO.findUsersByOrgUnitIds(orgUnitIds, command.getWithoutUserIds());
 		List<UserDTO> userDTOs = new ArrayList<>();
 		for (User user : users) {
-			UserDTO userDTO = mapper().map(user, UserDTO.class, UserDTO.Mode.WITH_BASE_ORG_UNIT_AND_BASE_PROFILES);
+			UserDTO userDTO = mapper().map(user, new UserDTO(), UserDTO.Mode.WITH_BASE_PROFILES);
 			userDTO.generateCompleteName();
+
+			// Currently loaded OrgUnits doesn't have a link to their parent and to their children
+			// Let's force the UserDTO to have these links
+			if (user.getMainOrgUnitWithProfiles() != null) {
+				userDTO.setMainOrgUnit(mapper().map(user.getMainOrgUnitWithProfiles().getOrgUnit(), OrgUnitDTO.class));
+			}
+			List<OrgUnit> secondaryOrgUnits = user.getSecondaryOrgUnits();
+			if (secondaryOrgUnits != null && !secondaryOrgUnits.isEmpty()) {
+				userDTO.setSecondaryOrgUnits(mapper().mapCollection(secondaryOrgUnits, OrgUnitDTO.class));
+			}
+
 			userDTOs.add(userDTO);
 		}
 		return new ListResult<>(userDTOs);
