78d50505ea72a786a01d466abe45f770e413e389
Nikita Jibhkate <niksj1996@gmail.com>
2016-08-19 16:21:47 +0530

    Update LayoutGroupDTO.java

--- src/main/java/org/sigmah/shared/dto/layout/LayoutGroupDTO.java
+++ src/main/java/org/sigmah/shared/dto/layout/LayoutGroupDTO.java
@@ -3,7 +3,6 @@ package org.sigmah.shared.dto.layout;
 /*
  * #%L
  * Sigmah
- * %%
  * Copyright (C) 2010 - 2016 URD
  * %%
  * This program is free software: you can redistribute it and/or modify
@@ -21,7 +20,7 @@ package org.sigmah.shared.dto.layout;
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-
+import com.extjs.gxt.ui.client.data.BaseModelData;
 import java.util.List;
 
 import org.sigmah.client.util.ToStringBuilder;
@@ -33,102 +32,121 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * LayoutGroupDTO.
- * 
+ *
  * @author Denis Colliot (dcolliot@ideia.fr)
  */
 public class LayoutGroupDTO extends AbstractModelDataEntityDTO<Integer> {
 
-	/**
-	 * Serial version UID.
-	 */
-	private static final long serialVersionUID = 8520711106031085130L;
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = 8520711106031085130L;
 
-	/**
-	 * DTO corresponding entity name.
-	 */
-	public static final String ENTITY_NAME = "layout.LayoutGroup";
+    /**
+     * DTO corresponding entity name.
+     */
+    public static final String ENTITY_NAME = "layout.LayoutGroup";
 
-	// DTO attributes keys.
-	public static final String TITLE = "title";
-	public static final String ROW = "row";
-	public static final String COLUMN = "column";
-	public static final String PARENT_LAYOUT = "parentLayout";
-	public static final String CONSTRAINTS = "constraints";
+    // DTO attributes keys.
+    public static final String TITLE = "title";
+    public static final String ROW = "row";
+    public static final String COLUMN = "column";
+    public static final String PARENT_LAYOUT = "parentLayout";
+    public static final String CONSTRAINTS = "constraints";
+    public static final String CONTAINER = "container";
+    public static final String CONSTRAINT = "constraint";
+    public static final String IS_DISABLED = "isdisabled";
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String getEntityName() {
-		return ENTITY_NAME;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getEntityName() {
+        return ENTITY_NAME;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected void appendToString(final ToStringBuilder builder) {
-		builder.append(TITLE, getTitle());
-		builder.append(ROW, getRow());
-		builder.append(COLUMN, getColumn());
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void appendToString(final ToStringBuilder builder) {
+        builder.append(TITLE, getTitle());
+        builder.append(ROW, getRow());
+        builder.append(COLUMN, getColumn());
+    }
 
-	// Layout group title
-	public String getTitle() {
-		return get(TITLE);
-	}
+    // Layout group title
+    public String getTitle() {
+        return get(TITLE);
+    }
 
-	public void setTitle(String title) {
-		set(TITLE, title);
-	}
+    public void setTitle(String title) {
+        set(TITLE, title);
+    }
 
-	// Row index
-	public Integer getRow() {
-		return (Integer) get(ROW);
-	}
+    // Row index
+    public Integer getRow() {
+        return (Integer) get(ROW);
+    }
 
-	public void setRow(Integer row) {
-		set(ROW, row);
-	}
+    public void setRow(Integer row) {
+        set(ROW, row);
+    }
 
-	// Column index
-	public Integer getColumn() {
-		return (Integer) get(COLUMN);
-	}
+    // Column index
+    public Integer getColumn() {
+        return (Integer) get(COLUMN);
+    }
 
-	public void setColumn(Integer column) {
-		set(COLUMN, column);
-	}
+    public void setColumn(Integer column) {
+        set(COLUMN, column);
+    }
 
-	// Reference to layoutDTO
-	public LayoutDTO getParentLayout() {
-		return get(PARENT_LAYOUT);
-	}
+    // Reference to layoutDTO
+    public LayoutDTO getParentLayout() {
+        return get(PARENT_LAYOUT);
+    }
 
-	public void setParentLayout(LayoutDTO parentLayout) {
-		set(PARENT_LAYOUT, parentLayout);
-	}
+    public void setParentLayout(LayoutDTO parentLayout) {
+        set(PARENT_LAYOUT, parentLayout);
+    }
 
-	// Reference to layout group constraints list
-	public List<LayoutConstraintDTO> getConstraints() {
-		return get(CONSTRAINTS);
-	}
+    // Reference to layout group constraints list
+    public List<LayoutConstraintDTO> getConstraints() {
+        return get(CONSTRAINTS);
+    }
 
-	public void setConstraints(List<LayoutConstraintDTO> constraints) {
-		set(CONSTRAINTS, constraints);
-	}
+    public void setConstraints(List<LayoutConstraintDTO> constraints) {
+        set(CONSTRAINTS, constraints);
+    }
 
-	public Widget getWidget() {
-		final FieldSet fieldSet = new FieldSet();
-		fieldSet.setHeadingHtml(getTitle());
-		fieldSet.setCollapsible(true);
+    public BaseModelData getContainerModel() {
+        return get(CONTAINER);
+    }
 
-		final FormLayout formLayout = new FormLayout();
-		formLayout.setLabelWidth(250);
+    public void setContainerModel(BaseModelData model) {
+        set(CONTAINER, model);
+    }
 
-		fieldSet.setLayout(formLayout);
+    public LayoutConstraintDTO getConstraint() {
+        return get(CONSTRAINT);
+    }
 
-		return fieldSet;
-	}
+    public void setConstraint(LayoutConstraintDTO constraint) {
+        set(CONSTRAINT, constraint);
+    }
+
+    public Widget getWidget() {
+        final FieldSet fieldSet = new FieldSet();
+        fieldSet.setHeadingHtml(getTitle());
+        fieldSet.setCollapsible(true);
+
+        final FormLayout formLayout = new FormLayout();
+        formLayout.setLabelWidth(250);
+
+        fieldSet.setLayout(formLayout);
+
+        return fieldSet;
+    }
 
 }
