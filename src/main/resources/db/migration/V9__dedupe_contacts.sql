81dac32aa4e00feada220a5a405a60e3a608d218
François Tavin <fta@atolcd.com>
2016-09-01 11:26:06 +0200

    Remove pg_trgm extension creation in flyway script
    (refs #634)
    
    Change-Id: I72b7530333fa4ec28ea98038c97ddae77616e7bc

--- src/main/resources/db/migration/V9__dedupe_contacts.sql
+++ src/main/resources/db/migration/V9__dedupe_contacts.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
 CREATE INDEX contact_fullname_idx ON contact USING gin(LOWER(name||' '||firstname) gin_trgm_ops);
 CREATE INDEX contact_email_idx ON contact (LOWER(email));
 CREATE INDEX userlogin_fullname_idx ON userlogin USING gin(LOWER(name||' '||firstname) gin_trgm_ops);
