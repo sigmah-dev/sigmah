2730ead1129f758486535bb27901d136837e5b2a
François Tavin <fta@atolcd.com>
2017-08-25 11:33:26 +0200

    Retrieve Sigmah version using maven for docker-tomcat.sh
    Change-Id: Ia5ad5ac0140f27dacc3c4085a2fa618111435c11

--- docker-tomcat.sh
+++ docker-tomcat.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+SIGMAH_VERSION=`mvn --quiet -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec`
+
 exec docker run -it --rm \
             --link $(docker-compose ps -q postgresql):postgresql \
-            -v $PWD/target/sigmah-2.2-SNAPSHOT/:/usr/local/tomcat/webapps/sigmah/:ro \
+            -v $PWD/target/sigmah-${SIGMAH_VERSION}/:/usr/local/tomcat/webapps/sigmah/:ro \
             -p 8080:8080 \
             tomcat:8.0
